4338f91a8ee9794d312c22747218f85d604847ef
Przemysław Całka <przemekcalka1@outlook.com>
2020-04-06 04:59:22 +0200

    Create database

--- DatingSite.API/Startup.cs
+++ DatingSite.API/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingSite.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,9 +24,11 @@ namespace DatingSite.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container.x
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(context => context.UseSqlite
+            (Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
 
